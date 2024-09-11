cd4b80e79ae2437081e7deb73cd9236f3745d144
Jordan Thao <Jordan.thao23@gmail.com>
2024-09-11 10:11:57 -0600

    Variables_Exercise

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Declare and initialize variables
+string characterName = "Bennett";
+int characterAge = 19;
+char characterAdventurerRank = 'C';
+bool characterIsAdventurer = true;
+double characterCurrentFunds = 1.89;
+decimal characterRatePerRequest = 15.72m;
+
+// Writing the variables to the console
+Console.WriteLine($"This is {characterName} he is adventurer, {characterIsAdventurer}. He is {characterAge} and has an adventuring rank of {characterAdventurerRank}, though he only has the measly funds of {characterCurrentFunds} so he must take on requests to make some money. For each request he makes {characterRatePerRequest}.");
+    
